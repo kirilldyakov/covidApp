eb55dec1e796af6331de0bb8c60b0b5618840935
dkirill <a@b.c>
2021-01-19 00:08:08 +0300

    Добавлен Koin

--- app/src/main/java/ru/strongit/covidapp/CovidApp.kt
+++ app/src/main/java/ru/strongit/covidapp/CovidApp.kt
@@ -1,6 +1,28 @@
 package ru.strongit.covidapp
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import ru.strongit.covidapp.di.appComponent
 
-class CovidApp: Application() {
+
+class CovidApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        configureDi()
+    }
+
+    private fun configureDi() {
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@CovidApp)
+            modules(provideComponent())
+        }
+    }
+
+    private fun provideComponent() = appComponent
 }
---
+++ app/src/main/java/ru/strongit/covidapp/di/appComponent.kt
@@ -0,0 +1,12 @@
+package ru.strongit.covidapp.di
+
+import org.koin.core.module.Module
+
+val appComponent = listOf<Module>(
+//        repositoryModule,
+//
+//        createRemoteModule("http://192.168.1.40:8087/api/"),
+////        createRemoteModule("http://192.168.1.56:8080/api/"),
+//        featureBenzinModule,
+//        localModule
+)
